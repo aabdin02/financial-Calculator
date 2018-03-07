f59d3c52dc303a1570e6aeb8703fed8c33e74dd4
Dawit Abera <dawitabera64@gmail.com>
2018-03-07 14:30:44 -0800

    - Interface class created

---
+++ JavaApplication6/src/FinancialApp/Algorithm/FinancialAppCalculator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FinancialApp.Algorithm;
+
+/**
+ *
+ * @author dawitabera
+ */
+public interface FinancialAppCalculator {
+    public double calulateMonthlyPayment();
+    
+}
