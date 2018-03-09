500597ac6585c6784a53581844b9c718eb5db4e9
aabdin02 <aabdin02@seattlecentral.edu>
2018-03-09 14:12:00 -0800

    - Implemented the reset Button

--- AndriodFinancialCalculator/src/FinancialApp/Algorithm/FinancialAPP.java
+++ AndriodFinancialCalculator/src/FinancialApp/Algorithm/FinancialAPP.java
@@ -23,11 +23,12 @@ public class FinancialAPP {
             //1 vehPrice,  2 salesTaxPercent,3 feesTaxable,3taxFree, 4 downPayment,
             //5 tradeInPayment, 6 amountOwedTradeIn, 7 interestRatePercent, 8 loanTermMonths
                                     //1         2   3       3       4       5       6     7     8
-        AutoLoan loan = new AutoLoan(100000, 5.00, 2008.00, 5000.00, 2000.00, 2000.00,5000.00,6, 360);
+        AutoLoan loan = new AutoLoan(100000, 5.00, 000.00, 000.00, 000.00, 000.00, 000.00,6, 360);
         System.out.println("Monthly payment: "+loan.calulateMonthlyPayment());
-//        System.out.println("  Total Payment: "+loan.calculateTotalPayment());
-//        System.out.println(" Total Interest: "+loan.calculateInterest());
-//        System.out.println("Annual  Payment: "+loan.calculateAnnualPayment());
+        System.out.println("Amount Financed: "+loan.getAmountFinanced());
+        System.out.println(" Total Interest: "+loan.calculateInterest());
+        System.out.println("  Total Payment: "+loan.calculateTotalPayment());
+
 //        loan.calulateMonthlyPayment();
     }
     
