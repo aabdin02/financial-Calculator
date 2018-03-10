0ff6a611690639605fe5608b21ef6bf0c201ba00
aabdin02 <aabdin02@seattlecentral.edu>
2018-03-09 16:47:06 -0800

    - Connect AutoLoanUI to it Algorithm

--- AndriodFinancialCalculator/src/FinancialApp/Algorithm/FinancialAPP.java
+++ AndriodFinancialCalculator/src/FinancialApp/Algorithm/FinancialAPP.java
@@ -5,6 +5,8 @@
  */
 package FinancialApp.Algorithm;
 
+import java.util.ArrayList;
+import javafx.util.Pair;
 /**
  *
  * @author aabdin02
@@ -17,19 +19,27 @@ public class FinancialAPP {
     public static void main(String[] args) {
         
         int p = 12000;
-        double i = 0.05;
+//        double i = 0.05;
         int t = 5;
         
             //1 vehPrice,  2 salesTaxPercent,3 feesTaxable,3taxFree, 4 downPayment,
             //5 tradeInPayment, 6 amountOwedTradeIn, 7 interestRatePercent, 8 loanTermMonths
                                     //1         2   3       3       4       5       6     7     8
         AutoLoan loan = new AutoLoan(100000, 5.00, 000.00, 000.00, 000.00, 000.00, 000.00,6, 360);
-        System.out.println("Monthly payment: "+loan.calulateMonthlyPayment());
-        System.out.println("Amount Financed: "+loan.getAmountFinanced());
-        System.out.println(" Total Interest: "+loan.calculateInterest());
-        System.out.println("  Total Payment: "+loan.calculateTotalPayment());
-
-//        loan.calulateMonthlyPayment();
+//        System.out.println("Monthly payment: "+loan.calulateMonthlyPayment());
+//        System.out.println("Amount Financed: "+loan.getAmountFinanced());
+//        System.out.println(" Total Interest: "+loan.calculateInterest());
+//        System.out.println("  Total Payment: "+loan.calculateTotalPayment());
+       ArrayList<Pair<Double, Pair<Double,Double>>> ar = new ArrayList(loan.populateAmount());
+       boolean first = true;
+       for(int i = 0; (first || i < ar.size()  && ((Math.round(ar.get(i - 1).getKey()) * 100.00/100.00) != 0)); i++){
+           int month = i;
+           double amount = Math.round(ar.get(i).getKey()) * 100.00/100.00;
+           double principal = Math.round(ar.get(i).getValue().getKey()) * 100.00/100.00;
+           double interest = principal - amount;
+           double balance = Math.round(ar.get(i).getValue().getKey()) * 100.00/100.00;
+           first = false;
+       }
     }
     
 }
