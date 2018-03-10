54c0ac8d2ecfc6d48def17345b74b2aa07b0dba0
aabdin02 <aabdin02@seattlecentral.edu>
2018-03-09 20:59:15 -0800

    - Implemented Discount Algorithm

---
+++ AndriodFinancialCalculator/src/FinancialApp/Algorithm/Discount.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FinancialApp.Algorithm;
+
+/**
+ *
+ * @author aabdin02
+ */
+public class Discount {
+    private  double regularPrice;
+    private final double salesTax;
+    private double toPayPrice;
+    private final double percent;
+    private final double additional;
+    private final double additional1;
+    
+    public Discount(boolean off, double reg, double salesT, double per
+                    , double add, double add1){
+        this.regularPrice = reg;
+        this.additional = add;
+        this.salesTax= salesT;
+        this.percent = per;
+        this.additional1 = add1;
+        this.toPayPrice = reg - (reg * per)/100;
+        this.toPayPrice -= (add * toPayPrice)/100;
+        this.toPayPrice -= (add1 * toPayPrice)/100;
+    }
+   
+    
+    public double getSavings(){
+        return (regularPrice + getSalesTax() - getTotalPayment());
+    }
+    public double getSalesTax(){
+        return (toPayPrice * salesTax)/100;
+    }
+    public double getTotalPayment(){
+        return toPayPrice + getSalesTax();
+    }
+    
+    public double totalPercentOff(){
+        return (getSavings() * toPayPrice)/100;
+    }
+}
--- AndriodFinancialCalculator/src/FinancialApp/Algorithm/FinancialAPP.java
+++ AndriodFinancialCalculator/src/FinancialApp/Algorithm/FinancialAPP.java
@@ -25,21 +25,26 @@ public class FinancialAPP {
             //1 vehPrice,  2 salesTaxPercent,3 feesTaxable,3taxFree, 4 downPayment,
             //5 tradeInPayment, 6 amountOwedTradeIn, 7 interestRatePercent, 8 loanTermMonths
                                     //1         2   3       3       4       5       6     7     8
-        AutoLoan loan = new AutoLoan(100000, 5.00, 000.00, 000.00, 000.00, 000.00, 000.00,6, 360);
-//        System.out.println("Monthly payment: "+loan.calulateMonthlyPayment());
+//        AutoLoan loan = new AutoLoan(100000, 5.00, 000.00, 000.00, 000.00, 000.00, 000.00,6, 360);
+////        System.out.println("Monthly payment: "+loan.calulateMonthlyPayment());
 //        System.out.println("Amount Financed: "+loan.getAmountFinanced());
 //        System.out.println(" Total Interest: "+loan.calculateInterest());
 //        System.out.println("  Total Payment: "+loan.calculateTotalPayment());
-       ArrayList<Pair<Double, Pair<Double,Double>>> ar = new ArrayList(loan.populateAmount());
-       boolean first = true;
-       for(int i = 0; (first || i < ar.size()  && ((Math.round(ar.get(i - 1).getKey()) * 100.00/100.00) != 0)); i++){
-           int month = i;
-           double amount = Math.round(ar.get(i).getKey()) * 100.00/100.00;
-           double principal = Math.round(ar.get(i).getValue().getKey()) * 100.00/100.00;
-           double interest = principal - amount;
-           double balance = Math.round(ar.get(i).getValue().getKey()) * 100.00/100.00;
-           first = false;
-       }
+//       ArrayList<Pair<Double, Pair<Double,Double>>> ar = new ArrayList(loan.populateAmount());
+//       boolean first = true;
+//       for(int i = 0; (first || i < ar.size()  && ((Math.round(ar.get(i - 1).getKey()) * 100.00/100.00) != 0)); i++){
+//           int month = i;
+//           double amount = Math.round(ar.get(i).getKey()) * 100.00/100.00;
+//           double principal = Math.round(ar.get(i).getValue().getKey()) * 100.00/100.00;
+//           double interest = principal - amount;
+//           double balance = Math.round(ar.get(i).getValue().getKey()) * 100.00/100.00;
+//           first = false;
+//       }
+        Discount dis = new Discount(true, 100, 5, 5, 0, 0);
+        System.out.println("Sales Tax: " + dis.getSalesTax());
+        System.out.println("Total Payment: " + dis.getTotalPayment());
+        System.out.println("Savings: "+ dis.getSavings());
+        System.out.println("Total Percentage Of: " + dis.totalPercentOff());
     }
     
 }
